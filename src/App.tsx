51794d6ee772a5ed2b0b2daa5e0ccc5a2e9fa244
vladimir <lovkiy2012@gmail.com>
2021-05-04 21:40:19 +0300

    get bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import Card, { CardVariant } from './components/Card'
 const App = () => {
   return (
     <div>
-      <Card
-        variant={CardVariant.outlined}
-        height='200px'
-        width='200px'
-        onClick={() => console.log('click App')}
-      >
-        <button>btn</button>
-      </Card>
+      <div className='container'>
+        <Card
+          variant={CardVariant.outlined}
+          height='200px'
+          width='200px'
+          onClick={() => console.log('click App')}
+        >
+          <button className='btn btn-primary'>btn</button>
+        </Card>
+      </div>
     </div>
   )
 }
